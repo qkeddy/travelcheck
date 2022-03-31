d125001dd599814094c2e73380cb60b9ac9e19fb
Quin Eddy <qkeddy@gmail.com>
2022-03-31 17:50:08 -0400

    Removed unneccessary logging Removed unneccessary commented code Added comments where unneccessary Consistently applied comments to functions Removed the duplicative updatePage() function in the init() Applied Prettier formatting which consistently changed double quotes to single

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
 // Find global HTML elements on page
-var formEl = document.getElementById('countryList');
-var countryNameEl = document.getElementById('countryName');
-var flagEl = document.getElementById('flagImage');
-var casesPerMilEl = document.getElementById('casesPerMil')
-var newCasesEl = document.getElementById('newCases');
-var travelScoreEl = document.getElementById('advisoryNum');
-var savedCountriesEl = document.getElementById('savedQueries');
+var formEl = document.getElementById("countryList");
+var countryNameEl = document.getElementById("countryName");
+var flagEl = document.getElementById("flagImage");
+var casesPerMilEl = document.getElementById("casesPerMil");
+var newCasesEl = document.getElementById("newCases");
+var travelScoreEl = document.getElementById("advisoryNum");
+var savedCountriesEl = document.getElementById("savedQueries");
 var submitButtonEl = document.querySelector("button");
-var totalCasesEl = document.getElementById('totalCases');
-var covidTsEl = document.getElementById('covidTs');
-var travelTsEl = document.getElementById('travelTs');
-
-// Global Variables
-//var countryStats = [];
+var totalCasesEl = document.getElementById("totalCases");
+var covidTsEl = document.getElementById("covidTs");
+var travelTsEl = document.getElementById("travelTs");
 
 /**
- * TODO - Convert this to a click button operation
  * ! Function to add a country to the countryStats local storage
  */
 function addCountry(countryIso2) {
@@ -51,6 +47,7 @@ function addCountry(countryIso2) {
             travelScore: [], // A rolling list of 30 numbers for historical purposes
         };
 
+        // Read from local storage and fill in the countryStat object with COVID-19 data
         let covidData = JSON.parse(localStorage.getItem("covidData"));
         for (j = 0; j < covidData.length; j++) {
             if (countryStat.iso2 == covidData[j].countryInfo.iso2) {
@@ -67,15 +64,16 @@ function addCountry(countryIso2) {
             }
         }
 
-
-        console.log('populated with covid data', countryStat);
+        // Read from local storage and fill in the countryStat object with travel safety data
         let travelData = JSON.parse(localStorage.getItem("travelData"));
+
         // Get the ISO2 code
         const countryIso2c = countryStat.iso2;
 
         // Convert to epoch time
         let tempDate =
-            new Date(travelData[countryIso2c].advisory.updated).valueOf() / 1000;
+            new Date(travelData[countryIso2c].advisory.updated).valueOf() /
+            1000;
 
         // Set the respective values in the object
         countryStat.travelTs.unshift(tempDate);
@@ -83,13 +81,12 @@ function addCountry(countryIso2) {
             travelData[countryIso2c].advisory.score
         );
 
+        // Add the country data to the full saved list of country data
         countryStats.push(countryStat);
     }
-    //refreshCovidData();
-    console.log("here's the data to store", countryStats)
+
     // Write data to local storage
     writeLocalStorage(countryStats);
-    console.log("this is what's in local storage", readLocalStorage());
 }
 
 /**
@@ -98,38 +95,14 @@ function addCountry(countryIso2) {
 function refreshCovidData() {
     var casesUrl = "https://disease.sh/v3/covid-19/countries";
 
-    // Read local storage data
-    //let countryStats = readLocalStorage();
+    // Fetch COVID-19 data
     fetch(casesUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            localStorage.setItem('covidData', JSON.stringify(data))
-            // console.log(data);
-            // var covidStoredData = {
-            //     expiration: Date.now(),
-            //     data: data
-            // }
-            //for (i = 0; i < countryStats.length; i++) {
-            
-                // for (j = 0; j < data.length; j++) {
-                //     if (countryStat.iso2 == data[j].countryInfo.iso2) {
-                //         if (countryStat.flag === "") {
-                //             countryStat.flag = data[j].countryInfo.flag;
-                //         }
-                //         countryStat.name = data[j].country;
-                //         countryStat.covidTs.unshift(data[j].updated);
-                //         countryStat.todayCases.unshift(data[j].todayCases);
-                //         countryStat.totalCases.unshift(data[j].cases);
-                //         countryStat.totalCasesPerMillion.unshift(
-                //             data[j].casesPerOneMillion
-                //         );
-                //     }
-                // }
-            //}
-            // Write results to local storage
-            //writeLocalStorage(countryStats);
+            // Write fetched data to local storage
+            localStorage.setItem("covidData", JSON.stringify(data));
         });
 }
 
@@ -185,14 +158,10 @@ function addTravelData(countryStat) {
             travelData[countryIso2].advisory.score
         );
     }
-
-    // Write results to local storage
-    //writeLocalStorage(countryStats);
 }
 
 /**
- * ! Update page
- * << Update page with blended data >>
+ * ! Update page with blended data
  */
 function updatePage(newCountry) {
     var countryData = readLocalStorage();
@@ -201,32 +170,29 @@ function updatePage(newCountry) {
 }
 
 /**
- * ! Update Countries
- * << Update search history with list items >>
+ * ! Update search history with list items
  */
 function updateCountries(countryData) {
-    savedCountriesEl.innerHTML = '';
+    savedCountriesEl.innerHTML = "";
     for (i = 0; i < countryData.length; i++) {
-        var liEl = document.createElement('li');
+        var liEl = document.createElement("li");
         liEl.textContent = countryData[i].name;
-        liEl.setAttribute('data-iso2', countryData[i].iso2);
-        liEl.setAttribute('class', 'mdc-list-item');
-        liEl.addEventListener('click', handleClick);
+        liEl.setAttribute("data-iso2", countryData[i].iso2);
+        liEl.setAttribute("class", "mdc-list-item");
+        liEl.addEventListener("click", handleClick);
         savedCountriesEl.append(liEl);
-
     }
 }
 
 /**
- * ! Update Data
- * << Update travel score and covid data on page >>
+ * ! Update travel score and covid data on page
  */
 function updateData(iso2) {
     var countryStats = readLocalStorage();
-    for (i=0; i <countryStats.length; i++) {
+    for (i = 0; i < countryStats.length; i++) {
         if (countryStats[i].iso2 == iso2) {
             countryNameEl.textContent = countryStats[i].name;
-            flagEl.setAttribute('src', countryStats[i].flag);
+            flagEl.setAttribute("src", countryStats[i].flag);
             totalCasesEl.textContent = countryStats[i].totalCases[0];
             casesPerMilEl.textContent = countryStats[i].totalCasesPerMillion[0];
 
@@ -234,30 +200,32 @@ function updateData(iso2) {
             newCasesEl.textContent = todayCases;
 
             var covidDay = countryStats[i].covidTs[0];
-            covidTsEl.textContent = moment(covidDay).format('MM-DD-YYYY');
+            covidTsEl.textContent = moment(covidDay).format("MM-DD-YYYY");
 
             travelScoreEl.textContent = countryStats[i].travelScore[0];
-            
+
             var travelDay = countryStats[i].travelTs[0];
-            travelTsEl.textContent = moment.unix(travelDay).format('MM-DD-YYYY');
+            travelTsEl.textContent = moment
+                .unix(travelDay)
+                .format("MM-DD-YYYY");
         }
     }
     return;
 }
 
 /**
- * ! Handle click
- * << Handle click event listener >>
+ * ! Handle click event listener
  */
 function handleClick(event) {
-    var iso2 = event.target.getAttribute('data-iso2');
+    var iso2 = event.target.getAttribute("data-iso2");
     updateData(iso2);
 }
 
-
+/**
+ * ! Populate the country dropdown list
+ */
 function populateList() {
-
-    var casesUrl = "https://disease.sh/v3/covid-19/countries"
+    var casesUrl = "https://disease.sh/v3/covid-19/countries";
 
     fetch(casesUrl)
         .then(function (response) {
@@ -265,31 +233,28 @@ function populateList() {
         })
         .then(function (data) {
             console.log(data.length);
-            for (i=0; i < data.length; i++) {
-                if (data[i].countryInfo.lat !== 0){ //generate option tag                                   
-                    var optionEl = document.createElement("option")
-                    //set option value to data[0].countryInfo.iso2          
-                    optionEl.value = data[i].countryInfo.iso2
-                    //set option text context to data[0].country            
-                    optionEl.textContent = data[i].country
-                    //append to optgroup where label === data[0].continent  
+            for (i = 0; i < data.length; i++) {
+                if (data[i].countryInfo.lat !== 0) {
+                    //generate option tag
+                    var optionEl = document.createElement("option");
+                    //set option value to data[0].countryInfo.iso2
+                    optionEl.value = data[i].countryInfo.iso2;
+                    //set option text context to data[0].country
+                    optionEl.textContent = data[i].country;
+                    //append to optgroup where label === data[0].continent
                     rootEl = document.getElementById(data[i].continent);
-                    rootEl.append(optionEl); }
-                               
+                    rootEl.append(optionEl);
+                }
             }
-        })
+        });
 }
-
 
 /**
  * ! Reads from local storage and updates the countryData object that can be used by other functions
  */
 function readLocalStorage() {
     // Fetch data from local storage and save in local variable
-
     return JSON.parse(localStorage.getItem("countryStats"));
-
-    //return storedData;
 }
 
 /**
@@ -304,39 +269,30 @@ function writeLocalStorage(countryStats) {
  * ! Initialization function
  */
 function init() {
-    populateList()
     // Pull latest travel safety data and update CountryStats
-    refreshTravelSafetyData(); // Comment out when testing
+    refreshTravelSafetyData();
 
     // Pull the latest COVID-19 data and update countryStats
     refreshCovidData();
 
-    // Update the page
-    //updatePage();
-    readLocalStorage();
     // Populates country list
-    populateList()
+    populateList();
 }
 
 // Run init routine
 init();
 
-
-// Event listener to select a certain country
-submitButtonEl.addEventListener("click", function(event) {
+/**
+ * ?Event listener to select a certain country
+ */
+submitButtonEl.addEventListener("click", function (event) {
     // Override default HTML form behavior
     event.preventDefault();
 
-    // TODO - temporary - add a country to countryStats in local storage. The hardcoded string will be replaced with the selected country from the dropdown.
-    //addCountry("AL");
+    // Pass the selected country to the addCountry function
     var newCountry = formEl.value;
     addCountry(newCountry);
 
-    // Add the travel safety to CountryStats
-    //addTravelData(); // Comment out when testing
-
-    // Pull the latest COVID-19 data and update countryStats
-    //refreshCovidData();
-
+    // Populates country list
     updatePage(newCountry);
 });
